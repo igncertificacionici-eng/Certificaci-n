4c3a81fbefce7abbbd86c4d6cb49c51c820a8cd6
igncertificacionici-eng <igncertificacionici@gmail.com>
2026-04-13 10:07:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ document.getElementById("btnEnviar").addEventListener("click", async () => {
   const formData = new FormData();
   formData.append("codigo", codigo);
   formData.append("tipo", tipo);
-  formData.append("archivo", archivo);
+  formData.append("archivo", archivo); 
+
 
   try {
     const response = await fetch("https://script.google.com/macros/s/AKfycbzCOkTdnTfA4GIQvmjrzJ2OLRPFjVeiz5ECqEHd6kPXhmqHRQjdXP9tQ7M6rrcT5TZlFw/exec", {
